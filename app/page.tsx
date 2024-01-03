5f4e32e42d7f9cb88176bbc92cac6ba01daf8e2b
Wageesh Arya <wageesharya114@gmail.com>
2024-01-03 17:46:27 +0900

    updated parent page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,27 @@
 import Image from "next/image";
 import Nav from "./components/Nav/Nav";
 import Main from "./components/Main/Main";
+import Skills from "./components/Skills/Skills";
+import Experience from "./components/Experience/Experience";
+import { Suspense } from "react";
+import { ScrollControls } from "@react-three/drei";
+import Contact from "./components/Contact/Contact";
 export default function Home() {
   return (
     <main>
-      <Nav />
-      <Main />
+      <Suspense
+        fallback={
+          <div className="w-[100vw] h-[100vh] flex items-center justify-center">
+            Loading!
+          </div>
+        }
+      >
+        <Nav />
+        <Main />
+        <Skills />
+        <Experience />
+        <Contact />
+      </Suspense>
     </main>
   );
 }
