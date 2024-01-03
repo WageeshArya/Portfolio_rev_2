2f973a2ca2737dbee08650b9c22b83ce298cf520
Wageesh Arya <wageesharya114@gmail.com>
2024-01-03 17:46:37 +0900

    created fonts file

---
+++ app/helpers/fonts.tsx
@@ -0,0 +1,11 @@
+// import { Silkscreen, Space_Mono } from "next/font/google";
+import { Space_Mono, Silkscreen } from "next/font/google";
+
+export const space_mono = Space_Mono({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+export const silkscreen = Silkscreen({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
