8eb1b0742102a4788838514db433a9c38df370dc
Wageesh Arya <wageesharya114@gmail.com>
2024-01-07 16:24:51 +0900

    updated main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
+"use client";
 import Image from "next/image";
 import Nav from "./components/Nav/Nav";
-import Main from "./components/Main/Main";
+import { Main } from "./components/Main/Main";
 import Skills from "./components/Skills/Skills";
 import Experience from "./components/Experience/Experience";
-import { Suspense } from "react";
-import { ScrollControls } from "@react-three/drei";
+import { Suspense, useEffect } from "react";
 import Contact from "./components/Contact/Contact";
 export default function Home() {
+  useEffect(() => {
+    async () => {
+      const LocomotiveScroll = (await import("locomotive-scroll")).default;
+      const locomotiveScroll = new LocomotiveScroll();
+    };
+  });
+
   return (
     <main>
       <Suspense
