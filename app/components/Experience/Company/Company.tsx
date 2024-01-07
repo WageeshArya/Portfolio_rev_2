c18b61561a58b242785411639efd0970828691d0
Wageesh Arya <wageesharya114@gmail.com>
2024-01-07 16:26:29 +0900

    updated experience design

---
+++ app/components/Experience/Company/Company.tsx
@@ -0,0 +1,220 @@
+"use client";
+import React, { useEffect } from "react";
+import classes from "./Card.module.css";
+import gsap, { Quad } from "gsap";
+import Image from "next/image";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+type Props = {};
+
+const Company = ({ item, index }: any) => {
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.fromTo(
+      `.${item.company}-companyName`,
+      {
+        display: "none",
+        opacity: 0,
+        y: 50,
+        skewY: 2,
+      },
+      {
+        display: "block",
+        opacity: 1,
+        y: 0,
+        skewY: 0,
+        scrollTrigger: {
+          trigger: `#${item.company}`,
+          start: "top center",
+          end: "bottom center",
+          markers: false,
+          toggleActions: "restart none none reverse",
+        },
+      }
+    );
+    gsap.fromTo(
+      `.${item.company}-period`,
+      {
+        opacity: 0,
+        y: 50,
+        skewY: 2,
+      },
+      {
+        opacity: 1,
+        y: 0,
+        skewY: 0,
+        delay: 0.1,
+        scrollTrigger: {
+          trigger: `#${item.company}`,
+          start: "top center",
+          end: "bottom center",
+          markers: false,
+          toggleActions: "restart none none reverse",
+        },
+      }
+    );
+    gsap.fromTo(
+      `.${item.company}-position`,
+      {
+        opacity: 0,
+        y: 50,
+        skewY: 2,
+      },
+      {
+        opacity: 1,
+        y: 0,
+        skewY: 0,
+        delay: 0.2,
+        scrollTrigger: {
+          trigger: `#${item.company}`,
+          start: "top center",
+          end: "bottom center",
+          markers: false,
+          toggleActions: "restart none none reverse",
+        },
+      }
+    );
+    gsap.fromTo(
+      `.${item.company}-about`,
+      {
+        opacity: 0,
+        y: 50,
+        skewY: 2,
+      },
+      {
+        opacity: 1,
+        y: 0,
+        skewY: 0,
+        delay: 0.3,
+        scrollTrigger: {
+          trigger: `#${item.company}`,
+          start: "top center",
+          end: "bottom center",
+          markers: false,
+          toggleActions: "restart none none reverse",
+        },
+      }
+    );
+    gsap.fromTo(
+      `.${item.company}-skillsTitle`,
+      {
+        opacity: 0,
+        y: 50,
+        skewY: 2,
+      },
+      {
+        opacity: 1,
+        y: 0,
+        skewY: 0,
+        delay: 0.4,
+        scrollTrigger: {
+          trigger: `#${item.company}`,
+          start: "top center",
+          end: "bottom center",
+          markers: false,
+          toggleActions: "restart none none reverse",
+        },
+      }
+    );
+    gsap.fromTo(
+      `.${item.company}-skills`,
+      {
+        opacity: 0,
+        y: 50,
+        skewY: 2,
+      },
+      {
+        opacity: 1,
+        y: 0,
+        skewY: 0,
+        delay: 0.5,
+        scrollTrigger: {
+          trigger: `#${item.company}`,
+          start: "top center",
+          end: "bottom center",
+          markers: false,
+          toggleActions: "restart none none reverse",
+        },
+      }
+    );
+    gsap.fromTo(
+      `#${item.company}-logo`,
+      {
+        y: 120,
+        opacity: 0,
+      },
+      {
+        y: 0,
+        delay: 0.4,
+        duration: 1,
+        opacity: 1,
+        ease: "power4.out",
+        scrollTrigger: {
+          trigger: `#${item.company}`,
+          start: "top center",
+          end: "bottom center",
+          markers: false,
+          toggleActions: "restart none none reverse",
+        },
+      }
+    );
+  }, []);
+
+  return (
+    <div key={item.company} id={item.company}>
+      <div
+        className={`flex w-[60%] mx-auto ${
+          index % 2 === 0 ? "flex-row-reverse text-right" : "flex-row"
+        }`}
+      >
+        <div className="p-5 my-[5rem] flex-1">
+          <div className={`text-5xl overflow-y-hidden mb-1`}>
+            <div className={`${item.company}-companyName`}>
+              {item.companyName}
+            </div>
+          </div>
+          <div className={`text-md overflow-y-hidden mb-1`}>
+            <div className={`${item.company}-period`}>{item.period}</div>
+          </div>
+          <div className={`text-l overflow-y-hidden mb-1 font-bold`}>
+            <div className={`${item.company}-position`}>{item.position}</div>
+          </div>
+          <div className={`mt-5 overflow-y-hidden`}>
+            <div className={`${item.company}-about`}>{item.about}</div>
+          </div>
+          <div className="mt-4">
+            <div className="overflow-y-hidden font-bold">
+              <div className={`${item.company}-skillsTitle`}>
+                What I mostly worked with
+              </div>
+            </div>
+            <div className="overflow-y-hidden">
+              <div className={`${item.company}-skills`}>
+                {item.skills.join(", ")}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          className={`p-5 my-[5rem] flex items-center overflow-hidden justify-center ${
+            item.img ? "flex-1" : ""
+          }`}
+        >
+          {item.img && (
+            <div className="overflow-y-hidden">
+              <Image
+                id={`${item.company}-logo`}
+                className="object-cover"
+                src={item.img}
+                width={200}
+                height={200}
+                alt=""
+              />
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Company;
--- app/components/Experience/Experience.tsx
+++ app/components/Experience/Experience.tsx
@@ -1,20 +1,134 @@
-import React from "react";
-import Title from "./Title";
+import { useEffect } from "react";
 import { experience } from "@/app/helpers/experience";
-import Card from "./Card/Card";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Image from "next/image";
+import Company from "./Company/Company";
 
 type Props = {};
 
 const Experience = (props: Props) => {
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.fromTo(
+      "#expImg",
+      {
+        rotate: "45deg",
+        width: "200px",
+        height: "400px",
+      },
+      {
+        rotate: "0deg",
+        width: "100vw",
+        height: "75vh",
+        scrollTrigger: {
+          trigger: ".expander",
+          start: "top center",
+          end: "30% center",
+          markers: false,
+          scrub: 1,
+          toggleActions: "restart none none reverse",
+        },
+      }
+    );
+    gsap.fromTo(
+      "#titlePart1",
+      {
+        scale: 1,
+        x: 0,
+      },
+      {
+        scale: 10,
+        x: -3500,
+        scrollTrigger: {
+          trigger: ".expander",
+          start: "top center",
+          end: "bottom 10%",
+          markers: false,
+          scrub: 1,
+          toggleActions: "restart none none reverse",
+        },
+      }
+    );
+    gsap.fromTo(
+      "#titlePart1",
+      {
+        opacity: 1,
+      },
+      {
+        opacity: 0,
+        scrollTrigger: {
+          trigger: ".expander",
+          start: "center center",
+          end: "bottom center",
+          markers: false,
+          toggleActions: "restart none none reverse",
+        },
+      }
+    );
+    gsap.fromTo(
+      "#titlePart2",
+      {
+        scale: 1,
+        x: 0,
+      },
+      {
+        scale: 10,
+        x: 3500,
+        scrollTrigger: {
+          trigger: ".expander",
+          start: "top center",
+          end: "bottom 10%",
+          markers: false,
+          scrub: 1,
+          toggleActions: "restart none none reverse",
+        },
+      }
+    );
+    gsap.fromTo(
+      "#titlePart2",
+      {
+        opacity: 1,
+      },
+      {
+        opacity: 0,
+        scrollTrigger: {
+          trigger: ".expander",
+          start: "center center",
+          end: "bottom center",
+          markers: false,
+          toggleActions: "restart none none reverse",
+        },
+      }
+    );
+  }, []);
+
   return (
-    <div>
-      <Title />
-      <div>
-        {experience.map((item: any, index: number) => {
-          return <Card key={item.company} item={item} index={index} />;
-        })}
+    <section>
+      <div className="mb-[30vh]">
+        <div className="w-[100vw] h-[100vh]">
+          <div className="h-[50vh] w-full" />
+          <div className="sticky top-1/2 -translate-y-1/2 flex items-center justify-center pointer-events-none text-[7rem]">
+            <Image
+              id="expImg"
+              className="absolute object-cover"
+              src={"/experience.svg"}
+              height={400}
+              width={200}
+              alt=""
+            />
+            <div id="titlePart1">MY EXPE</div>
+            <div id="titlePart2">RIENCE</div>
+          </div>
+          <div className="expander w-[100vw] h-[100vh]">
+            <div className="w-full h-[60vh]"></div>
+          </div>
+        </div>
       </div>
-    </div>
+      {experience.map((item: any, index: number) => {
+        return <Company key={item.company} item={item} index={index} />;
+      })}
+    </section>
   );
 };
 
