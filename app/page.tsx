bc2c5cbbe78f55f7be503cff8cbcd1afdc4642ba
Wageesh Arya <wageesharya114@gmail.com>
2024-01-08 14:17:14 +0900

    updated main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,45 @@
 "use client";
-import Image from "next/image";
 import Nav from "./components/Nav/Nav";
 import { Main } from "./components/Main/Main";
 import Skills from "./components/Skills/Skills";
 import Experience from "./components/Experience/Experience";
-import { Suspense, useEffect } from "react";
+import { useEffect, useState } from "react";
 import Contact from "./components/Contact/Contact";
+import Loader from "./components/Loader/Loader";
+import gsap from "gsap";
 export default function Home() {
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     async () => {
       const LocomotiveScroll = (await import("locomotive-scroll")).default;
       const locomotiveScroll = new LocomotiveScroll();
     };
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+    gsap.fromTo(
+      "#main",
+      {
+        opacity: 0,
+        delay: 1.5,
+        duration: 1,
+      },
+      {
+        opacity: 1,
+      }
+    );
   });
 
   return (
     <main>
-      <Suspense
-        fallback={
-          <div className="w-[100vw] h-[100vh] flex items-center justify-center">
-            Loading!
-          </div>
-        }
-      >
+      <Loader />
+      <div id="main" className={`${loading ? "!overflow-y-hidden" : ""}`}>
         <Nav />
         <Main />
         <Skills />
         <Experience />
         <Contact />
-      </Suspense>
+      </div>
     </main>
   );
 }
