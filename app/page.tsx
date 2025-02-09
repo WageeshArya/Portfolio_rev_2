dcea5a4712fd4826b28ca19e0fa55a9cd13c8793
Wageesh Arya <wageesharya114@gmail.com>
2025-02-09 22:55:50 +0900

    feat: updated page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,17 @@
 "use client";
-import Nav from "./components/Nav/Nav";
-import { Main } from "./components/Main/Main";
-import Skills from "./components/Skills/Skills";
-import Experience from "./components/Experience/Experience";
-import { useEffect } from "react";
-import Contact from "./components/Contact/Contact";
-import Loader from "./components/Loader/Loader";
-import gsap from "gsap";
-export default function Home() {
-  useEffect(() => {
-    async () => {
-      const LocomotiveScroll = (await import("locomotive-scroll")).default;
-      const locomotiveScroll = new LocomotiveScroll();
-    };
-    document.body.style.overflowY = "hidden";
-    setTimeout(() => {
-      document.body.style.overflowY = "auto";
-    }, 3000);
-    gsap.fromTo(
-      "#main",
-      {
-        opacity: 0,
-        delay: 1.5,
-        duration: 1,
-      },
-      {
-        opacity: 1,
-      }
-    );
-  });
 
+import { Navbar } from "./components/molecules/Navbar";
+import { Experience } from "./components/organisms/Experience";
+import { Landing } from "./components/organisms/Landing";
+import { Skills } from "./components/organisms/Skills";
+
+export default function Home() {
   return (
     <main>
-      <Loader />
-      <div id="main">
-        <Nav />
-        <Main />
-        <Skills />
-        <Experience />
-        <Contact />
-      </div>
+      <Navbar />
+      <Landing />
+      <Skills />
+      <Experience />
     </main>
   );
 }
