fd31defe117ff89fce9c6d3471d2a338910e10fb
Wageesh Arya <wageesharya114@gmail.com>
2025-02-11 15:46:07 +0900

    fix: divided navbar

--- app/components/molecules/Navbar.tsx
+++ app/components/molecules/Navbar.tsx
@@ -7,11 +7,11 @@ export const Navbar = () => {
       id="navbar"
       className="fixed opacity-0 top-0 left-0 px-12 py-6 w-full flex items-center justify-center z-[1000]"
     >
-      <div className="flex justify-between px-5 py-3 w-full backdrop-filter backdrop-blur-[8px] backdrop-saturate-[200%] bg-[rgba(255,_255,_255,_0.4)] rounded-[12px] border-[1px] border-solid border-[rgba(255,255,255,0.125)]">
-        <div>
+      <div className="flex justify-between px-5 w-full">
+        <div className="px-4 py-3 backdrop-filter backdrop-blur-[8px] backdrop-saturate-[200%] bg-[rgba(255,_255,_255,_0.4)] rounded-[12px] border-[1px] border-solid border-[rgba(255,255,255,0.125)]">
           <AnchorLink link="#" text="WageeshArya." />
         </div>
-        <div className="flex">
+        <div className="flex px-4 py-3 backdrop-filter backdrop-blur-[8px] backdrop-saturate-[200%] bg-[rgba(255,_255,_255,_0.4)] rounded-[12px] border-[1px] border-solid border-[rgba(255,255,255,0.125)]">
           <div>
             <LanguageSelector />
           </div>
