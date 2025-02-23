62e6ef33818ba96a1a6fba029dccacbe3e0ca4bd
Wageesh Arya <wageesharya114@gmail.com>
2025-02-23 20:49:23 +0900

    feat: en and jp language text

---
+++ app/utils/language.ts
@@ -0,0 +1,203 @@
+export const textsEn = {
+  name: "Wageesh Arya",
+  html: {
+    thanks: "Thank you for checking out my portfolio! 👋",
+    intro: "Please check out these too",
+  },
+  landing: {
+    heading1: "FULL STACK\nWEB\nDEVELOPER",
+    subheading1: "w/ experience developing production AI applications.",
+    heading2: "WAGEESH ARYA",
+    subheading2:
+      "I’m a full stack developer with 4.5 years of experience developing robust,\nscalable, and efficient web applications.",
+  },
+  skills: {
+    title: "SKILLS",
+    FE: "Front End",
+    BE: "Back End",
+    others: "Others",
+  },
+  experience: {
+    title: "EXPERIENCE",
+    techbae: {
+      position: "Full Stack Engineer",
+      skillsRow1: ["React.js", "Redux", "Vue.js", "Nuxt.js", "SCSS"],
+      skillsRow2: [
+        "Node.js",
+        "Express",
+        "MongoDB",
+        "Mongoose",
+        "JWT Authentication",
+      ],
+      companyName: "Techbae",
+      period: "December 2020 to January 2021",
+      location: "Australia (Remote)",
+      description:
+        "I worked as a full stack developer, handling diverse projects and crafting custom content management systems (CMS) for various websites. My role involved comprehensive development tasks, ensuring seamless functionality and user-friendly interfaces.",
+    },
+    zenius: {
+      position: "Front End Engineer",
+      skillsRow1: ["React.js", "TypeScript", "SCSS", "Redux"],
+      skillsRow2: ["GraphQL", "WebRTC"],
+      companyName: "Zenius",
+      period: "December 2020 to January 2021",
+      location: "Indonesia (Remote)",
+      description:
+        "I served as a front-end developer for an Indonesian education platform, where my responsibilities included maintaining a group video call system integral to the platform's functionality. This role allowed me to contribute significantly to the user interface and enhance the overall user experience for the educational community.",
+    },
+    teraoka: {
+      position: "Full Stack Engineer",
+      skillsRow1: ["React.js", "TypeScript", "SCSS", "Redux"],
+      skillsRow2: [
+        "PHP",
+        "Kotlin/Android Studio",
+        "Fastlane",
+        "GitHub Actions",
+      ],
+      companyName: "Teraoka Seiko",
+      period: "July 2021 to September 2023",
+      location: "Tokyo, Japan",
+      description:
+        'The first company I worked at in Japan, I worked on maintaining a smartphone app "Shop&Go". I worked on the content management system on the front end, the development of some aspects of the android version of the app, and created an automated release workflow for the app on both the Apple App Store and the Android Play Store.',
+    },
+    bitA: {
+      position: "Technical Lead, Full Stack Engineer",
+      skillsRow1: ["Next.js", "TypeScript", "SCSS", "Redux", "MicroCMS"],
+      skillsRow2: [
+        "Node.js",
+        "Express",
+        "SQL",
+        "PostgreSQL",
+        "k6 Stress Testing",
+      ],
+      skillsRow3: [
+        "GitHub Actions",
+        "LangChain (AI development)",
+        "Azure OpenAI",
+        "Custom made Vector Stores",
+        "AWS",
+      ],
+      companyName: "BitA",
+      period: "October 2023 to Now",
+      location: "Tokyo, Japan",
+      description:
+        "In my current position, I work as a Technical lead of the front end, and a full-stack engineer on multiple projects.",
+    },
+  },
+  contact: {
+    links: "Links",
+    github: "GitHub",
+    linkedIn: "LinkedIn",
+    resume: "Resumé",
+    title: "Contact me - ",
+    cta: "Contact me @ \n wageesharya114@gmail.com",
+  },
+  nav: {
+    name: "WageeshArya.",
+    skills: "Skills",
+    experience: "Experience",
+    contact: "Contact",
+  },
+};
+
+export const textsJp = {
+  name: "アリャー ワギーシ",
+  html: {
+    thanks: "ポートフォリオをご覧いただき、ありがとうございます！ 🙇",
+    intro: "以下もぜひご確認ください！",
+  },
+  landing: {
+    heading1: "フルスタック\nエンジニア",
+    subheading1: "AIソリューションの実務開発経験もあります。",
+    heading2: "アリャー ワギーシ",
+    subheading2:
+      "4年半の実務経験を持つフルスタックエンジニアです。\nよろしくお願いいたします。",
+  },
+  skills: {
+    title: "スキル",
+    FE: "フロントエンド",
+    BE: "バックエンド",
+    others: "その他",
+  },
+  experience: {
+    title: "職歴",
+    techbae: {
+      position: "フルスタックエンジニア",
+      skillsRow1: ["React.js", "Redux", "Vue.js", "Nuxt.js", "SCSS"],
+      skillsRow2: [
+        "Node.js",
+        "Express",
+        "MongoDB",
+        "Mongoose",
+        "JWT Authentication",
+      ],
+      companyName: "Techbae Pty. Ltd.",
+      period: "2020年12月 〜 2021年1月",
+      location: "オーストラリア（リモート）",
+      description:
+        "フルスタック開発者として勤務し、多様なプロジェクトに携わり、さまざまなWebサイト向けにカスタムCMS（コンテンツ管理システム）を開発しました。私の役割には、包括的な開発業務が含まれており、シームレスな機能とユーザーフレンドリーなインターフェースを実現することを重視していました。",
+    },
+    zenius: {
+      position: "フロントエンドエンジニア",
+      skillsRow1: ["React.js", "TypeScript", "SCSS", "Redux"],
+      skillsRow2: ["GraphQL", "WebRTC"],
+      companyName: "株式会社Zenius",
+      period: "2021年1月 〜 2021年7月",
+      location: "インドネシア（リモート）",
+      description:
+        "インドネシアの教育プラットフォームでフロントエンド開発者として勤務し、プラットフォームの重要な機能であるグループビデオ通話システムの保守を担当しました。この役割を通じて、ユーザーインターフェースの向上に大きく貢献し、教育コミュニティ全体のユーザー体験を改善することができました。",
+    },
+    teraoka: {
+      position: "フルスタックエンジニア",
+      skillsRow1: ["React.js", "TypeScript", "SCSS", "Redux"],
+      skillsRow2: [
+        "PHP",
+        "Kotlin/Android Studio",
+        "Fastlane",
+        "GitHub Actions",
+      ],
+      companyName: "株式会社寺岡精工",
+      period: "2021年7月 〜 2023年10月",
+      location: "東京都大田区",
+      description:
+        "日本で最初に勤務した会社で、スマートフォンアプリ「Shop&Go」の保守に携わりました。フロントエンドのコンテンツ管理システムの開発や、アプリのAndroid版の一部機能の開発を担当し、さらにApple App StoreとGoogle Play Storeの両方でアプリを自動リリースするワークフローを構築しました。",
+    },
+    bitA: {
+      position: "テックリード・フルスタックエンジニア",
+      skillsRow1: ["Next.js", "TypeScript", "SCSS", "Redux", "MicroCMS"],
+      skillsRow2: [
+        "Node.js",
+        "Express",
+        "SQL",
+        "PostgreSQL",
+        "k6 Stress Testing",
+      ],
+      skillsRow3: [
+        "GitHub Actions",
+        "LangChain (AI development)",
+        "Azure OpenAI",
+        "Custom made Vector Stores",
+        "AWS",
+      ],
+      companyName: "株式会社ビットエー",
+      period: "2023年9月 〜 現在",
+      location: "東京都品川区",
+      description:
+        "現在の職務では、フロントエンドのテクニカルリードとして働くとともに、複数のプロジェクトでフルスタックエンジニアとしても従事しています。",
+    },
+  },
+  contact: {
+    links: "関連リンク",
+    github: "GitHub",
+    linkedIn: "LinkedIn",
+    resume: "履歴書",
+    title: "お問い合わせー",
+    cta: "ご連絡は以下のメールアドレスにてお願いいたします。\nwageesharya114@gmail.com",
+  },
+  nav: {
+    name: "アリャー ワギーシ",
+    skills: "スキル",
+    experience: "職歴",
+    contact: "お問い合わせ",
+  },
+};
